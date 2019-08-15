fed6b3a4238ca34ae295a1e6a1fd970699f168bb
autumn-ragland <raglanda998@gmail.com>
2019-08-15 09:51:53 -0500

    Graded 08-15-2019 09:49:47

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # JavaScript_SwitchCase_cw
 
+Good work. 2/2 correct. Score : 5 points. 
+
 ### Problem 1:
 Put the start of your program in a main function. Ask the user to enter a number to print. Create a switch case that prints 1 if you enter 1, 2 if you enter 2, 3 if you enter 3, 4 if you enter 4, and 5 if you enter 5.
 
--- index.js
+++ index.js
@@ -3,10 +3,7 @@
 // Put the start of your program in a main function. Ask the user to enter a number to print. Create a switch case that prints 1 if you enter 1, 2 if you enter 2, 3 if you enter 3, 4 if you enter 4, and 5 if you enter 5.
 // ### Problem 1:
 // Put the start of your program in a main function. Ask the user to enter a number to print. Create a switch case that prints 1 if you enter 1, 2 if you enter 2, 3 if you enter 3, 4 if you enter 4, and 5 if you enter 5.
-
-
 let userInput = prompt("Enter a number to print");
-
 switch (userInput) {
     case "1":
         console.log(1);
@@ -25,31 +22,28 @@ switch (userInput) {
         break;
     default:
         alert("enter a number between 1 - 5");
-
-
 }
 // ## Problem 2:
 // Put the start of your program in a main function. Ask the user to enter a 3 digit binary number. Create a switch case that prints 1 if you enter 1, 2 if you enter 10, 3 if you enter 11, 4 if you enter 100, and 5 if you enter 101.
+let userInput2 = prompt("Enter up to a 3 digit binary number");
 
-// let userInput = prompt("Enter up to a 3 digit binary number");
-//
-// switch (userInput) {
-//     case "1":
-//         console.log(1);
-//         break;
-//     case "10":
-//         console.log(2);
-//         break;
-//     case "11":
-//         console.log(3);
-//         break;
-//     case "100":
-//         console.log(4);
-//         break;
-//     case "101":
-//         console.log(5);
-//         break;
-//     default:
-//         alert("not a binary number");
-//
-// }
+switch (userInput2) {
+    case "1":
+        console.log(1);
+        break;
+    case "10":
+        console.log(2);
+        break;
+    case "11":
+        console.log(3);
+        break;
+    case "100":
+        console.log(4);
+        break;
+    case "101":
+        console.log(5);
+        break;
+    default:
+        alert("not a binary number");
+
+}
